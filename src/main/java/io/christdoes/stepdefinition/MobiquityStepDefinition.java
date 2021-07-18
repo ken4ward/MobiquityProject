ce50a43556a8e3f934f972f550354e962a5d747a
Kehinde Adeoya <ken4ward@yahoo.com>
2021-07-18 08:21:19 +0100

    Added all

--- src/main/java/io/christdoes/stepdefinition/MobiquityStepDefinition.java
+++ src/main/java/io/christdoes/stepdefinition/MobiquityStepDefinition.java
@@ -3,25 +3,26 @@ package io.christdoes.stepdefinition;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.christdoes.test.testcases.TestCases;
 
 public class MobiquityStepDefinition {
     @When("^Search for the user with username “Delphine”\\.$")
-    public void searchForTheUserWithUsernameDelphine() {
+    public void searchForTheUserWithUsernameDelphine() throws Throwable {
+        TestCases.searchForUsername();
     }
 
     @And("^Use the details fetched to make a search for the posts written by the user\\.$")
-    public void useTheDetailsFetchedToMakeASearchForThePostsWrittenByTheUser() {
+    public void useTheDetailsFetchedToMakeASearchForThePostsWrittenByTheUser() throws Throwable {
+        TestCases.searchForUserPost();
     }
 
     @And("^For each post, fetch the comments and validate if the emails in the comment section are in the proper format\\.$")
-    public void forEachPostFetchTheCommentsAndValidateIfTheEmailsInTheCommentSectionAreInTheProperFormat() {
+    public void forEachPostFetchTheCommentsAndValidateIfTheEmailsInTheCommentSectionAreInTheProperFormat() throws Throwable {
+        TestCases.searchForUserCommentinnPostAndVlidateEmail();
     }
 
-    @And("^Think of various scenarios for the test workflow, all the things that can go wrong\\. Add them to test coverage$")
-    public void thinkOfVariousScenariosForTheTestWorkflowAllTheThingsThatCanGoWrongAddThemToTestCoverage() {
-    }
-
-    @Then("^teardown test as it is completed\\.$")
-    public void teardownTestAsItIsCompleted() {
+    @Then("^Think of various scenarios for the test workflow, all the things that can go wrong\\. Add them to test coverage$")
+    public void thinkOfVariousScenariosForTheTestWorkflowAllTheThingsThatCanGoWrongAddThemToTestCoverage() throws Throwable {
+        TestCases.otherScenariosThatCouldGoWrong();
     }
 }
