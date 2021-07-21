6983fa2694e3adf5ba866706e83b1ace53462ae6
Kehinde Adeoya <ken4ward@yahoo.com>
2021-07-21 16:36:53 +0100

    Added all

--- src/main/java/io/christdoes/test/testcases/TestCases.java
+++ src/main/java/io/christdoes/test/testcases/TestCases.java
@@ -1,8 +1,15 @@
 package io.christdoes.test.testcases;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.christdoes.pojo.comments.CommentsItem;
+import io.christdoes.pojo.posts.PostsItem;
+import io.christdoes.pojo.todo.Todo;
+import io.christdoes.pojo.todo.TodoItem;
+import io.christdoes.pojo.users.UserItem;
 import io.christdoes.test.init.Init;
 import io.christdoes.utility.MyLogger;
 import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
@@ -10,12 +17,14 @@ import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static io.christdoes.test.init.Init.getProperties;
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.proxy;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
@@ -23,14 +32,20 @@ import org.apache.logging.log4j.LogManager;
 public class TestCases extends MyLogger {
     static int userId;
    static java.util.List<Integer> idList;
+    private static Object ArrayList;
 
     @Test(priority = 1)
     public static void searchForUsername( ) throws Throwable {
         Response response = RestAssured.given().queryParam(getProperties().getProperty("project.query"), getProperties().getProperty("project.username"))
                 .get(Init.initProperties() +getProperties().getProperty("users.pathURL"));
         response.then().contentType(ContentType.JSON).statusCode(200).log().all();
-        userId = (Integer) response.jsonPath().getList("id").get(0);
-        System.out.println("This is the user ID" +userId);
+        List<UserItem> userItems = response.as(new TypeRef<>() {});
+        for ( UserItem u: userItems ) {
+            if ( (Integer) response.jsonPath().getList("id").get(0) == u.getId() ){
+                userId = u.getId();
+                MyLogger.info("This is getting the user ID of Delphine " + userId);
+            }
+        }
     }
 
     /***
@@ -44,13 +59,13 @@ public class TestCases extends MyLogger {
         Response response = myRequest().get(Init.initProperties() + getProperties().getProperty("post.pathURL") + userId);
         // Get Response Body
         ResponseBody body = response.getBody();
-        String bodyStringValue = body.asString();
         idList = response.jsonPath().getList("id");
-        MyLogger.info("This is the user post IDs" +idList);
         response.then().contentType(ContentType.JSON).statusCode(200).log().all();
-        MyLogger.info("Response returns a valid code of  " +response.statusCode());
-        Assert.assertTrue(bodyStringValue.contains("id"));
-        MyLogger.info("It validates the IDs of the post are returned" +bodyStringValue.contains("id"));
+        List<PostsItem> postsItems = response.as(new TypeRef<>() {});
+        for ( PostsItem p : postsItems) {
+            Assert.assertTrue(p.getUserId() == userId);
+            MyLogger.info("This is the user Id of Delpihne " +userId);
+        }
     }
 
     /****
@@ -64,11 +79,12 @@ public class TestCases extends MyLogger {
         for ( Integer e: idList) {
             MyLogger.info("Iteration through the post IDs passed " + e);
             Response response = myRequest().get( Init.initProperties()+getProperties().getProperty("user.comment.pathURL") +e);
-            List<String> email = response.jsonPath().getList("email");
-            MyLogger.info( "The emails are retrieved using the IDs" + email );
-            for (String s: email) {
+            response.then().contentType(ContentType.JSON).statusCode(200).log().all();
+            List<CommentsItem> commentsItems = response.as(new TypeRef<>() {});
+            MyLogger.info( "The emails are retrieved using the IDs" + commentsItems );
+            for (CommentsItem s: commentsItems) {
                 MyLogger.info("email validations are carried out here " +s);
-                Assert.assertTrue(validate(s));
+                Assert.assertTrue(validate(s.getEmail()));
             }
         }
     }
@@ -81,10 +97,13 @@ public class TestCases extends MyLogger {
     public static void otherScenariosThatCouldGoWrong() throws Throwable {
         Response response = myRequest().get(Init.initProperties() + getProperties().getProperty("user.todo.pathURL") + userId);
         // Get Response Body
-        ResponseBody body = response.getBody();
-        String bodyStringValue = body.asString();
-        idList = response.jsonPath().getList("title");
-        Assert.assertTrue(bodyStringValue.contains("title"));
+        response.then().contentType(ContentType.JSON).statusCode(200).log().all();
+        List<TodoItem> todoItems = response.as(new TypeRef<>() {});
+        Assert.assertNotNull(response.body());
+        for (TodoItem s: todoItems) {
+            Assert.assertTrue(s.getUserId() == userId);
+            MyLogger.info(s.toString());
+        }
     }
 
     private static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
@@ -97,10 +116,6 @@ public class TestCases extends MyLogger {
         RequestSpecification httpRequest = given();
         httpRequest.header("Content-Type", "application/json");
         return httpRequest;
-    }
-
-    public static void negativeTestCases(){
-
     }
 
 }
